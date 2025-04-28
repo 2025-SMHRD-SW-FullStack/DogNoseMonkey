e4573215cb96debe1228adf010f49d54ab76f135
HJM <smjm0850@gmail.com>
2025-04-28 15:14:09 +0900

    기능추가

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -7,6 +7,8 @@ public class TeamClass {
 		
 		System.out.println("아무 숫자나 입력 : ");
 		int num = sc.nextInt();
+		System.out.println("한번 더 입력하시오 >> ");
+		int num1 = sc.nextInt();
 		
 		System.out.println("팀장 첫번째");
 		System.out.println("서명우 바보");  
@@ -14,5 +16,6 @@ public class TeamClass {
 		System.out.println("김승혁 바보");
 		System.out.println("Team 개코원숭이");
 		System.out.println(num);
+		System.out.println(num1);
 	}
 }
