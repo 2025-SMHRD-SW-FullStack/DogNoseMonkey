e90f8ff85f66434e6d258e7f8b12b63eba517d34
김승혁 <peter9889466@gmail.com>
2025-04-28 15:37:28 +0900

    KSHbranch

---
+++ Ex02/Ex02/src/TeamClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TeamClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("아무 숫자나 입력 : ");
+		int num = sc.nextInt();
+		System.out.println("한번 더 입력하시오 >> ");
+		int num1 = sc.nextInt();
+		
+		System.out.println("팀장 첫번째");
+		System.out.println("서명우 바보");  
+		System.out.println("홍준모 바보");
+		System.out.println("김승혁 바보");
+		System.out.println("Team 개코원숭이");
+		System.out.println(num);
+		System.out.println(num1);
+	}
+}
