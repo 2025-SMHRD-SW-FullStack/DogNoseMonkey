b1d012426658e3d026806ad329497f95f7a6d381
HJM <smjm0850@gmail.com>
2025-04-28 15:00:16 +0900

    오타수정

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -5,7 +5,7 @@ public class TeamClass {
 		System.out.println("팀장 첫번째");
 		System.out.println("서명우 바보");  
 		System.out.println("홍준모 바보");
-		System.out.println("김승형 바보");
+		System.out.println("김승혁 바보");
 		System.out.println("Team 개코원숭이");
 	}
 }
