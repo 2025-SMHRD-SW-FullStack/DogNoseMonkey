a68a197a214053a7accbfb71e43291d5dda45909
김승혁 <peter9889466@gmail.com>
2025-04-28 15:12:03 +0900

    숫자 입력 추가

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
 
 public class TeamClass {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("아무 숫자나 입력 : ");
+		int num = sc.nextInt();
+		
 		System.out.println("팀장 첫번째");
 		System.out.println("서명우 바보");  
 		System.out.println("홍준모 바보");
 		System.out.println("김승혁 바보");
 		System.out.println("Team 개코원숭이");
+		System.out.println(num);
 	}
 }
