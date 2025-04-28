232b7e7ed501dd712d174fbcf8534d6ecfed8cd9
HJM <smjm0850@gmail.com>
2025-04-28 12:53:05 +0900

    변경

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -3,12 +3,9 @@ public class TeamClass {
 
 	public static void main(String[] args) {
 		System.out.println("팀장 첫번째");
-<<<<<<< HEAD
 		System.out.println("팀원 1 두번째");  
-=======
 		System.out.println("팀원 1 두번째");
 		System.out.println("팀원 2 세번째");
 		System.out.println("팀원 3 네번째");
->>>>>>> fa0bb8fd122c28c88fc133839d598fa6b4d9f522
 	}
 }
