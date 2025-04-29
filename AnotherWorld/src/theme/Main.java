c79db5b64cbe994103170153fe35de2ce5411628
HJM <smjm0850@gmail.com>
2025-04-29 18:21:03 +0900

    world

--- AnotherWorld/src/theme/Main.java
+++ AnotherWorld/src/theme/Main.java
@@ -1,10 +1,17 @@
 package theme;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println();
-
+		System.out.println("어느 평화로운 밤 혼자서 캠핑을 즐기고 있는 username");
+		try {
+		    Thread.sleep(10000); 
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+		System.out.println("어느 평화로운 밤 혼자서 캠핑을 즐기고 있는 username");
 	}
 
 }
