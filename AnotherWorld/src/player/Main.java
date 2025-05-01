8ca27fcdf21362c6e077d24fa49cb554869433f4
HJM <smjm0850@gmail.com>
2025-05-01 15:51:13 +0900

    타이머까지

--- AnotherWorld/src/player/Main.java
+++ AnotherWorld/src/player/Main.java
@@ -1,12 +1,12 @@
 package player;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		PlayerController playerController = new PlayerController();
+		QuizMain quizMain = null;
 //        playerController.registerPlayer();  // 회원가입 진행
 //        playerController.closeScanner();    // 사용 후 Scanner 닫기
 
@@ -15,7 +15,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int num = 0;
 		while (true) {
-			System.out.println("[1]회원가입 [2]로그인 [3]랭킹조회 [4]게임시작 [0]종료");
+			System.out.println("[1]회원가입 [2]로그인 [0]종료");
 			System.out.print(">> ");
 			num = sc.nextInt();
 			if (num == 1) {
@@ -24,24 +24,35 @@ public class Main {
 			else if(num == 2) {
 				boolean succes = playerController.login();
 				if (succes) {
-					break;
+                    PlayerDTO currentPlayer = playerController.getCurrentPlayer();
+                    if (currentPlayer != null) {
+                        quizMain = new QuizMain(currentPlayer);
+                        break;
+                    }
 				}
-			} 
-			else if(num == 3) {
+			} else if(num == 0) {
+				System.out.println("게임을 종료합니다");
+				return;
+			}
+		}
+		
+		while(true) {
+			System.out.println("[1]랭킹조회 [2]게임시작 [3]회원탈퇴 [0]종료");
+			System.out.print(">> ");
+			num = sc.nextInt();
+			if(num == 1) {
 				playerController.Ranking();
-			} 
-			else if(num == 4) {
-				playerController.quiz();
-			}
-			else if(num == 11) {
+			} else if(num == 2) {
+				if (quizMain != null) {
+                    quizMain.StartGame(); // 퀴즈 게임 시작
+                }
+			} else if(num == 3) {
 				playerController.delete();
-			}
-			else if(num == 0) {
+			} else if(num == 0) {
 				System.out.println("게임을 종료합니다");
 				break;
 			}
 		}
-		
-		
+		sc.close();
 	}
 }
--- AnotherWorld/src/player/QuizDAO.java
+++ AnotherWorld/src/player/QuizDAO.java
@@ -1,4 +1,4 @@
-package Quiz;
+package player;
 
 import java.util.ArrayList;
 import java.util.List;
